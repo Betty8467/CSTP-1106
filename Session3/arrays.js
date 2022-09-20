b91cd468f2b8291737f6f16b80917f1140eee39c
Betty Merid <betlehemmeridg@gmail.com>
2022-09-20 12:18:29 -0700

    dfz

---
+++ Session3/arrays.js
@@ -0,0 +1,6 @@
+let studentList = []; //There is no data inside
+let students = ["Kevin", "David"]; //
+let list = ["Karen", 30, true] //array with multiple
+
+list[0]; //"Kevin"
+list[10]; //undefined
---
+++ Session3/variable.js
@@ -0,0 +1,27 @@
+function sum(){
+    let a = 10;
+    console.log(b, "Value of b");
+    let c = 10;
+
+    if(true){
+        var b = 11;
+        let c = 10;
+        const d = 20;
+    }
+
+    // console.log(a); //10
+    // console.log(b); //11
+    // console.log(c);
+    // console.log(d);
+}
+
+sum();
+
+function random(){
+    let a = 5;
+    if (true){
+    console.log(a);
+    }
+}
+
+random();
