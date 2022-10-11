edf7fd6a697477a42bb23aae2c5e8a6d250c4169
Betty Merid <betlehemmeridg@gmail.com>
2022-10-11 12:32:20 -0700

    Update index.js

--- Session6/Express Server/index.js
+++ Session6/Express Server/index.js
@@ -2,15 +2,32 @@ const express = require('express');
 
 const app = express();
 const PORT = 4000;
+const student = [];
+
+//Telling express that the data I am getting is of type JSON
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send("Welcome");
+    res.send("Welcome to srudent APIs");
 })
 
-app.get('/user', (req, res) => {
-    res.send("Hello User!");
+// We are trying to post student data in the server
+app.get('/students', (req, res) => {
+    //For get response we give 200 response
+    //const data = res.status(200);
+    //return data.json(student);
+    return res.status(200).json(student);
+})
+
+
+app.post('/students', (re, res) => {
+
+    const studentData = req.body;
+    student.push(studentData);
+    //201 is used when you are trying to 
+
 })
 
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
